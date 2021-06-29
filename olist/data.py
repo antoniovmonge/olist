cd7faab1c75bb93869adfc2b9abfe2579a2737cd
Antonio Vilchez <antoniovmonge@gmail.com>
2021-06-29 11:51:56 +0200

    continue exploration in Notebook and implemented funtion merge table

--- olist/data.py
+++ olist/data.py
@@ -9,10 +9,10 @@ class Olist:
         Its values should be pandas.DataFrame loaded from csv files
         """
 
-        # root_dir = os.path.dirname(os.path.dirname(__file__))
-        # csv_path = os.path.join(root_dir, "raw_data", "csv")
+        root_dir = os.path.dirname(os.path.dirname(__file__))
+        csv_path = os.path.join(root_dir, "raw_data", "csv")
 
-        csv_path = ('../raw_data/csv')
+        # csv_path = ('../raw_data/csv')
         # Create the list file_names
         file_names = [f for f in os.listdir(csv_path) if f.endswith('.csv')]
 
@@ -24,4 +24,23 @@ class Olist:
         for k,f in zip(key_names, file_names):
             data[k] = pd.read_csv(os.path.join(csv_path, f))
         return data
+
+    def get_matching_table(self):
+        """
+        This function returns a matching table between
+        columns [ "order_id", "review_id", "customer_id", "product_id", "seller_id"]
+        """
+
+        data = self.get_data()
+
+        # Selecting columns of interest
+        orders = data['orders'][['customer_id', 'order_id']]
+        reviews = data['order_reviews'][['order_id', 'review_id']]
+        items = data['order_items'][['order_id', 'product_id', 'seller_id']]
+
+        # Merge DataFrame
+        matching_table = orders\
+            .merge(reviews, on='order_id', how='outer')\
+            .merge(items, on='order_id', how='outer')
         
+        return matching_table
